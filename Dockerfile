1b79460c5fbcf69fdaf9b89c3a42d94101d32f67
Franz Nastor <franznastor@gmail.com>
2024-05-29 11:35:13 -0500

    clearing out some comments

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM python:3.12.3-bullseye
+MAINTAINER franz@nakamotomining.ai
 
 RUN apt-get update && \
     apt-get install -y tmux npm curl libhdf5-dev && \
@@ -16,18 +17,8 @@ COPY README.md /app
 COPY requirements.txt /app
 COPY src /app/src
 
-COPY startup-script.sh /app
-
-#RUN python -m venv local
+#COPY startup-script.sh /app
 
 RUN pip install --no-cache-dir -r requirements.txt
 
-#RUN chmod +x /app/startup_script.sh
-
 CMD ["/bin/bash", "-c", "sleep infinity"]
-# last arg is the context
-# docker build -t NAME -f DOCKERFILE . 
-
-# docker run -it NAME bash 
-
-#cd localvenv
--- startup-script.sh
+++ startup-script.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
 activate () {
-  source localvenv/bin/activate
   echo "Starting the server on port ${PORT}"
   which python
   pm2 start "python src/openscope/miner/event_subscription.py" --name real_time_events
-  comx module register ff_openscope-miner0 ${COMMUNE_KEY} --netuid 5
-  pm2 start "comx module serve python src/openscope/miner/signal_trade.py --ip 0.0.0.0 --port ${PORT}" --name openscope_serve_miner
-  pm2 start "python src/openscope/miner/IQ50.py" --name openscope_trading_miner
-  pm2 logs
+  comx module register <name-of-your-miner> ${COMMUNE_KEY} --netuid 5
 }
 
 activate
