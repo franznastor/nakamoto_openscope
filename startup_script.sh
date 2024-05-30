3dd60318ad10f7678c866281698cfa52d3cb970e
Franz Nastor <franznastor@gmail.com>
2024-05-30 17:36:09 -0500

    executable

--- startup_script.sh
+++ startup_script.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#!/bin/bash
-
 set -e  # Exit immediately if a command exits with a non-zero status
 set -o pipefail  # Return the exit status of the last command in the pipeline that failed
 
